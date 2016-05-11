4c93af8097f209a987aefd5c070c7d7f1ca93d2a
koinhug@126.com <koinhug@126.com>
2016-05-11 21:49:57 +0800

    添加面试, web前端内容

---
+++ web前端/ajax/ajaxdemo/test.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<h1>这是一个测试页面：
+    <?php echo "Hello World!"?>
+    </h1>
+</body>
+</html>
